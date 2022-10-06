e225c94fc9f1bb49e15df8ccacf9e78f9f14c18a
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-10-06 16:16:08 +0200

    placeholder

--- src/scripts/components/select.js
+++ src/scripts/components/select.js
@@ -1,5 +1,6 @@
 const selectComponentsWrapper = document.querySelector('[data-pos-component="Select"]');
 if (selectComponentsWrapper) {
+  const placeholder = selectComponentsWrapper.querySelector('.placeholder');
   selectComponentsWrapper.querySelectorAll('[type="checkbox"]').forEach(checkbox => {
     checkbox.addEventListener('change', (event) => {
       const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
@@ -11,6 +12,13 @@ if (selectComponentsWrapper) {
           tag.classList.add('hidden');
         }
       });
+
+      const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+      if (checkedBoxes.length) {
+        placeholder.classList.add('hidden');
+      } else {
+        placeholder.classList.remove('hidden');
+      }
     });
   });
 };
