048a6afe78f08974e296d25be4d9d5402e2a3ced
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-10-13 14:09:19 +0200

    mobile view

--- src/scripts/components/select.js
+++ src/scripts/components/select.js
@@ -37,33 +37,39 @@ selectComponentsWrappers.forEach(selectComponentsWrapper => {
     });
   });
 
-  /* simple select */
-  let optionChecked = "";
+  /* single select */
   const nativeSelect = selectComponentsWrapper.querySelector('.pos-select-native');
+  const singleSelectTagSelect = () => {
+    const tags = selectComponentsWrapper.querySelectorAll('.pos-select-custom__tags > div');
+    tags.forEach(tag => {
+      const tagValue = tag.getAttribute("data-value");
+      if (tagValue == nativeSelect.value) {
+        tag.classList.remove('hidden');
+      } else {
+        tag.classList.add('hidden');
+      }
+    })
 
+    if (nativeSelect.value) {
+      placeholder.classList.add('hidden');
+    } else {
+      placeholder.classList.remove('hidden');
+    }
+  }
+
+  /* single select - custom select*/
   selectComponentsWrapper.querySelectorAll('.pos-select-custom__options > div').forEach(option => {
     const value = option.getAttribute("data-value");
     option.addEventListener('click', () => {
       nativeSelect.value = value;
-      optionChecked = value;
       toggleOpen();
-
-      const tags = selectComponentsWrapper.querySelectorAll('.pos-select-custom__tags > div');
-      tags.forEach(tag => {
-        const tagValue = tag.getAttribute("data-value");
-        if (tagValue == value) {
-          tag.classList.remove('hidden');
-        } else {
-          tag.classList.add('hidden');
-        }
-      })
-
-      if (value) {
-        placeholder.classList.add('hidden');
-      } else {
-        placeholder.classList.remove('hidden');
-      }
+      singleSelectTagSelect();
     });
+  });
+
+  /* single select - native select*/
+  nativeSelect.addEventListener('change', (event) => {
+    singleSelectTagSelect();
   });
 
 });
