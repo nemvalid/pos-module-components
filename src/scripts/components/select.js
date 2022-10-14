2126ae3eead375424d62577a286d24317b7bd4e9
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-10-14 10:50:36 +0200

    fixes

--- src/scripts/components/select.js
+++ src/scripts/components/select.js
@@ -38,50 +38,53 @@ selectComponentsWrappers.forEach(selectComponentsWrapper => {
   });
 
   /* single select */
-  const nativeSelect = selectComponentsWrapper.querySelector('.pos-select-native');
-  const tagWrapper = selectComponentsWrapper.querySelector('.pos-select-custom__tags');
-  tagWrapper.addEventListener('click', toggleOpen);
-  const singleSelectTagSelect = () => {
-    const tags = selectComponentsWrapper.querySelectorAll('.pos-select-custom__tags > div');
-    tags.forEach(tag => {
-      const tagValue = tag.getAttribute("data-value");
-      if (tagValue == nativeSelect.value) {
-        tag.classList.remove('hidden');
-      } else {
-        tag.classList.add('hidden');
-      }
-    })
+  const nativeSelect = selectComponentsWrapper.querySelector('.pos-select-simple-native');
+  if (nativeSelect) {
+    console.log('nativeSelect',nativeSelect);
+    const tagWrapper = selectComponentsWrapper.querySelector('.pos-select-custom__tags');
+    tagWrapper.addEventListener('click', toggleOpen);
+    const singleSelectTagSelect = () => {
+      const tags = selectComponentsWrapper.querySelectorAll('.pos-select-custom__tags > div');
+      tags.forEach(tag => {
+        const tagValue = tag.getAttribute("data-value");
+        if (tagValue == nativeSelect.value) {
+          tag.classList.remove('hidden');
+        } else {
+          tag.classList.add('hidden');
+        }
+      })
 
-    const options = selectComponentsWrapper.querySelectorAll('.pos-select-custom__options > div');
-    options.forEach(option => {
-      const optionValue = option.getAttribute("data-value");
-      if (optionValue == nativeSelect.value) {
-        option.classList.add('bg-highlighted');
-      } else {
-        option.classList.remove('bg-highlighted');
-      }
-    })
+      const options = selectComponentsWrapper.querySelectorAll('.pos-select-custom__options > div');
+      options.forEach(option => {
+        const optionValue = option.getAttribute("data-value");
+        if (optionValue == nativeSelect.value) {
+          option.classList.add('bg-highlighted');
+        } else {
+          option.classList.remove('bg-highlighted');
+        }
+      })
 
-    if (nativeSelect.value) {
-      placeholder.classList.add('hidden');
-    } else {
-      placeholder.classList.remove('hidden');
+      if (nativeSelect.value) {
+        placeholder.classList.add('hidden');
+      } else {
+        placeholder.classList.remove('hidden');
+      }
     }
-  }
 
-  /* single select - custom select*/
-  selectComponentsWrapper.querySelectorAll('.pos-select-custom__options > div').forEach(option => {
-    const value = option.getAttribute("data-value");
-    option.addEventListener('click', () => {
-      nativeSelect.value = value;
-      toggleOpen();
+    /* single select - custom select*/
+    selectComponentsWrapper.querySelectorAll('.pos-select-custom__options > div').forEach(option => {
+      const value = option.getAttribute("data-value");
+      option.addEventListener('click', () => {
+        nativeSelect.value = value;
+        toggleOpen();
+        singleSelectTagSelect();
+      });
+    });
+
+    /* single select - native select*/
+    nativeSelect.addEventListener('change', (event) => {
       singleSelectTagSelect();
     });
-  });
-
-  /* single select - native select*/
-  nativeSelect.addEventListener('change', (event) => {
-    singleSelectTagSelect();
-  });
+  }
 
 });
