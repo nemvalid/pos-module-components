9d5ad2fdb21f92fa0c0ae270e8d2ceea32b50db5
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-10-24 15:09:30 +0200

    code clean

--- src/scripts/components/popup.js
+++ src/scripts/components/popup.js
@@ -1,34 +1,40 @@
+// opener buttons
 const popupOpeners = document.querySelectorAll('[data-popup-open-id]');
 popupOpeners.forEach(opener => {
   opener.addEventListener('click', (event) => {
-    const popupId= opener.getAttribute('data-popup-open-id');
-    const popup = document.querySelector(`[data-popup-id="${popupId}"]`);
-    const backdrop = document.querySelector(`[data-backdrop-id="${popupId}"]`);
-    popup.classList.toggle("hidden");
-    backdrop.classList.toggle("hidden");
-
-    // open
-    if (!popup.classList.contains('hidden')) {
-      popup.setAttribute("aria-hidden", false);
-      const firstInput = popup.querySelector('input');
-      if (firstInput) {
-        firstInput.focus();
-      }
-    // close
-    } else {
-      popup.setAttribute("aria-hidden", true);
-    }
+    const popupId = opener.getAttribute('data-popup-open-id');
+    togglePopup(popupId);
   });
 });
 
 const popups = document.querySelectorAll('[data-popup-id]');
 popups.forEach(popup => {
+  const popupId = popup.getAttribute('data-popup-id');
+  // popup close by ESC
   popup.addEventListener("keydown", event => {
     if (event.keyCode === 27) {
-      const popupId = popup.getAttribute('data-popup-id');
-      const backdrop = document.querySelector(`[data-backdrop-id="${popupId}"]`);
-      popup.classList.toggle("hidden");
-      backdrop.classList.toggle("hidden");
+      togglePopup(popupId);
     }
   });
 });
+
+
+
+const togglePopup = popupId => {
+  const popup = document.querySelector(`[data-popup-id="${popupId}"]`);
+  const backdrop = document.querySelector(`[data-backdrop-id="${popupId}"]`);
+  popup.classList.toggle("hidden");
+  backdrop.classList.toggle("hidden");
+
+  // open
+  if (!popup.classList.contains('hidden')) {
+    popup.setAttribute("aria-hidden", false);
+    const firstInput = popup.querySelector('input');
+    if (firstInput) {
+      firstInput.focus();
+    }
+  // close
+  } else {
+    popup.setAttribute("aria-hidden", true);
+  }
+}
