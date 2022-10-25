d8a8ba392ee6d102a673dfd72a348724923c6377
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-10-25 09:27:39 +0200

    auto open

--- src/scripts/components/popup.js
+++ src/scripts/components/popup.js
@@ -1,31 +1,3 @@
-// opener buttons
-const popupOpeners = document.querySelectorAll('[data-popup-open-id]');
-popupOpeners.forEach(opener => {
-  opener.addEventListener('click', (event) => {
-    event.stopPropagation();
-    const popupId = opener.getAttribute('data-popup-open-id');
-    togglePopup(popupId);
-  });
-});
-
-const popups = document.querySelectorAll('[data-popup-id]');
-popups.forEach(popup => {
-  const popupId = popup.getAttribute('data-popup-id');
-  // popup close by ESC
-  popup.addEventListener("keydown", event => {
-    if (event.keyCode === 27) {
-      closePopup(popupId);
-    }
-  });
-  // click out
-  document.addEventListener("click", (event) => {
-    const didClickedOutside = !popup.contains(event.target);
-    if (didClickedOutside) {
-      closePopup(popupId);
-    }
-  });
-});
-
 const togglePopup = (popupId, forceClose = false) => {
   const popup = document.querySelector(`[data-popup-id="${popupId}"]`);
   const backdrop = document.querySelector(`[data-backdrop-id="${popupId}"]`);
@@ -53,3 +25,36 @@ const togglePopup = (popupId, forceClose = false) => {
 const closePopup = popupId => {
   togglePopup(popupId, true);
 }
+
+// opener buttons
+const popupOpeners = document.querySelectorAll('[data-popup-open-id]');
+popupOpeners.forEach(opener => {
+  opener.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const popupId = opener.getAttribute('data-popup-open-id');
+    togglePopup(popupId);
+  });
+});
+
+const popups = document.querySelectorAll('[data-popup-id]');
+popups.forEach(popup => {
+  const popupId = popup.getAttribute('data-popup-id');
+  const closed = popup.getAttribute('data-closed');
+  // popup close by ESC
+  popup.addEventListener("keydown", event => {
+    if (event.keyCode === 27) {
+      closePopup(popupId);
+    }
+  });
+  // click out
+  document.addEventListener("click", (event) => {
+    const didClickedOutside = !popup.contains(event.target);
+    if (didClickedOutside) {
+      closePopup(popupId);
+    }
+  });
+  // auto-open
+  if (closed === "false") {
+    togglePopup(popupId);
+  }
+});
