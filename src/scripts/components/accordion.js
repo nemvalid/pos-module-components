b0b8a403a6599f6afc788a2473b9fe84b5bba070
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-11-04 15:19:21 +0100

    toggle

--- src/scripts/components/accordion.js
+++ src/scripts/components/accordion.js
@@ -1,12 +1,13 @@
 const accordionWrappers = document.querySelectorAll('[data-pos-component="Accordion"]');
 accordionWrappers.forEach(accordionWrapper => {
   const titles = accordionWrapper.querySelectorAll('.pos-accordion__title');
-  //  const panel = accordionWrapper.querySelector('.pos-accordion__panel');
 
   titles.forEach(title => {
     title.addEventListener('click', () => {
       const panel = title.nextElementSibling;
+      const icon = title.firstElementChild;
       panel.classList.toggle("hidden");
+      icon.classList.toggle("rotate-180");
     });
   })
 
