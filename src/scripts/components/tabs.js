f2666065699708b1ffcb6be2f36f8375cadf022a
Ocskó Gábor <ocsko.gabor@lexunit.hu>
2022-11-17 09:46:57 +0100

    tab change

--- src/scripts/components/tabs.js
+++ src/scripts/components/tabs.js
@@ -1,16 +1,25 @@
-const tablistWrappers = document.querySelectorAll('.pos-tabs__tablist');
+const tablistWrappers = document.querySelectorAll('[data-pos-component="Tabs"]');
 tablistWrappers.forEach(tablistWrapper => {
   const tabs = tablistWrapper.querySelectorAll('button');
+  const panels = tablistWrapper.querySelectorAll('.pos-tabs__panel');
   let firstTab = null;
-  tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-      tabs.forEach(tab => {
-        tab.classList.remove("pos-tabs__tab--active");
+  tabs.forEach((currentTab, currentTabIndex) => {
+    currentTab.addEventListener('click', () => {
+      tabs.forEach((tab, index) => {
+        if (currentTab === tab) {
+          tab.classList.add("pos-tabs__tab--active");
+          panels[index].classList.remove("hidden");
+        } else {
+          tab.classList.remove("pos-tabs__tab--active");
+          panels[index].classList.add("hidden");
+        }
       });
-      tab.classList.add("pos-tabs__tab--active");
+
     });
+    panels[currentTabIndex].classList.add("hidden");
     if (!firstTab) {
-      firstTab = tab;
+      firstTab = currentTab;
+      panels[currentTabIndex].classList.remove("hidden");
     }
   });
   firstTab.classList.add("pos-tabs__tab--active");
