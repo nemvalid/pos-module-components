5a78dbb23127f1f4855383e1804d9a4609131de8
Peter Almasi <Peter_Almasi@outlook.com>
2023-04-24 09:31:51 +0200

    remove duplication

--- src/components.js
+++ src/components.js
@@ -1,6 +1,5 @@
 /* The main entry file for the production (components) bundle */
 import './styles/components.css';
-import './scripts/components/flash';
 import './scripts/components/select';
 import './scripts/components/popup';
 import './scripts/components/accordion';
