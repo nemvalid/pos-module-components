6d642dcfec51422a85a5a10f3143e8ff6236a54e
Peter Almasi <Peter_Almasi@outlook.com>
2023-04-24 10:05:28 +0200

    get rid of dynamic imports and use component map

--- src/components.js
+++ src/components.js
@@ -1,21 +1,22 @@
 /* The main entry file for the production (components) bundle */
 import './styles/components.css';
-import './scripts/components/select';
-import './scripts/components/popup';
-import './scripts/components/accordion';
-import './scripts/components/tabs';
-import './scripts/components/more-less';
-import './scripts/components/dropdown';
 import './scripts/components/flash';
 
-const COMPONENT_MODULES = [
- 'select',
- 'popup',
- 'accordion',
- 'tabs',
- 'more-less',
- 'dropdown'
-];
+import select from './scripts/components/select';
+import popup from './scripts/components/popup';
+import accordion from './scripts/components/accordion';
+import tabs from './scripts/components/tabs';
+import moreless from './scripts/components/moreless';
+import dropdown from './scripts/components/dropdown';
+
+const COMPONENT_MODULES = {
+ 'select': select,
+ 'popup': popup,
+ 'accordion': accordion,
+ 'tabs': tabs,
+ 'moreless': moreless,
+ 'dropdown': dropdown
+};
 
 window.posComponents = window.posComponents || {};
 
@@ -24,16 +25,19 @@ const initialize = async (component) => {
   if (Array.isArray(component)) {
     components = component;
   } else if (!component) {
-    components = COMPONENT_MODULES;
+    components = Object.keys(COMPONENT_MODULES);
   } else {
     components.push(component);
   }
 
   for (component of components) {
+    if (!COMPONENT_MODULES[component]) {
+      console.warn(`${component} module is not implemented`);
+      continue;
+    }
     const elements = document.querySelectorAll(`[data-pos-component="${component}"]:not([data-pos-initialized])`);
     if (elements.length) {
-      const componentModule = await import(`./scripts/components/${component}`);
-      componentModule.default(elements);
+      COMPONENT_MODULES[component](elements);
       Array.from(elements).map(e => e.setAttribute('data-pos-initialized', true));
     }
   }
