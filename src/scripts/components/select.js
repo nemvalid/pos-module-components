32ef35c0bbe4a4d248c544a3d388e2b6645655ea
Łukasz Krysiewicz <zx@secondgate.pl>
2023-04-24 13:39:20 +0200

    Filtering the options list in custom select component (#124)
    * Filtering the options list
    
    * Fixed keyboard navigation and clearing filter when select closed
    
    * Removed maps
    
    * Updated before master merge

--- src/scripts/components/select.js
+++ src/scripts/components/select.js
@@ -1,7 +1,10 @@
 const componentModule = (elements) => {
+
   elements.forEach(selectComponentWrapper => {
     const placeholder = selectComponentWrapper.querySelector('.pos-select__placeholder');
     const opener = selectComponentWrapper.querySelector('.pos-select__opener');
+    const popup = selectComponentWrapper.querySelector('.pos-select__popup');
+    const filter = selectComponentWrapper.querySelector('.pos-select__filter');
     const options = selectComponentWrapper.querySelector('.pos-select__options');
     const head = selectComponentWrapper.querySelector('.pos-select__head');
 
@@ -10,19 +13,28 @@ const componentModule = (elements) => {
     /* toggle open for custom select */
     const toggleOpen = (event) => {
       event.stopPropagation();
-      options.classList.toggle("hidden");
+      popup.classList.toggle('hidden');
+      popup.ariaHidden = popup.ariaHidden === 'true' ? 'false' : 'true';
       const openerIcons = selectComponentWrapper.querySelectorAll('.pos-select__opener > div');
-      openerIcons[0].classList.toggle("hidden");
-      openerIcons[1].classList.toggle("hidden");
+      openerIcons[0].classList.toggle('hidden');
+      openerIcons[1].classList.toggle('hidden');
+
+      if(popup.ariaHidden === 'true' && filter){
+        filterClear();
+      }
     };
 
     const closeOptions = (event) => {
       event.stopPropagation();
-      options.classList.add("hidden");
+      popup.classList.add('hidden');
+      popup.ariaHidden = true;
       const openerIcons = selectComponentWrapper.querySelectorAll('.pos-select__opener > div');
-      openerIcons[0].classList.remove("hidden");
-      openerIcons[1].classList.add("hidden");
+      openerIcons[0].classList.remove('hidden');
+      openerIcons[1].classList.add('hidden');
       updateCustomSelectHovered(-1);
+      if(filter){
+        filterClear();
+      }
     };
 
     const watchClickOutside = (event) => {
@@ -37,11 +49,11 @@ const componentModule = (elements) => {
       const option = options.children[newIndex];
 
       if (prevOption) {
-        prevOption.classList.remove("bg-highlighted");
+        prevOption.classList.remove('bg-highlighted');
       }
       if (option) {
-        option.scrollIntoView({behavior: "smooth", block: "center"});
-        option.classList.add("bg-highlighted");
+        option.scrollIntoView({behavior: 'smooth', block: 'center'});
+        option.classList.add('bg-highlighted');
       }
 
       optionHoveredIndex = newIndex;
@@ -49,7 +61,7 @@ const componentModule = (elements) => {
 
     const supportKeyboardNavigation = (event) => {
       // press down -> go next
-      if (event.keyCode === 40 && optionHoveredIndex < options.children.length - 1 ) {
+      if (event.keyCode === 40 && optionHoveredIndex < options.querySelectorAll('.pos-select__option:not(.hidden)').length - 1) {
         event.preventDefault(); // prevent page scrolling
         updateCustomSelectHovered(optionHoveredIndex + 1);
       }
@@ -58,19 +70,24 @@ const componentModule = (elements) => {
       if (event.keyCode === 38 && optionHoveredIndex > 0) {
         event.preventDefault(); // prevent page scrolling
         updateCustomSelectHovered(optionHoveredIndex - 1);
+      } else if (event.keyCode === 38 && filter && optionHoveredIndex === 0){
+        filter.focus();
       }
 
       // press Enter or space -> select the option
       if (event.keyCode === 13 || event.keyCode === 32) {
-        event.preventDefault();
-        if (optionHoveredIndex == -1) {
-          toggleOpen(event);
-        } else {
-          const checkbox = options.children[optionHoveredIndex].querySelector('[type="checkbox"]');
-          if (checkbox) {
-            checkbox.click();
+        // don't react if user focused the filtering input
+        if(!document.activeElement.matches('input')){
+          event.preventDefault();
+          if (optionHoveredIndex == -1) {
+            toggleOpen(event);
           } else {
-            options.children[optionHoveredIndex].click();
+            const checkbox = options.children[optionHoveredIndex].querySelector('[type="checkbox"]');
+            if (checkbox) {
+              checkbox.click();
+            } else {
+              options.children[optionHoveredIndex].click();
+            }
           }
         }
       }
@@ -82,10 +99,10 @@ const componentModule = (elements) => {
     };
 
     head.addEventListener('click', toggleOpen);
-    document.addEventListener("click", watchClickOutside);
+    document.addEventListener('click', watchClickOutside);
     opener.addEventListener('click', toggleOpen);
     placeholder.addEventListener('click', toggleOpen);
-    selectComponentWrapper.addEventListener("keydown", supportKeyboardNavigation);
+    selectComponentWrapper.addEventListener('keydown', supportKeyboardNavigation);
 
 
     /* multiselect */
@@ -113,7 +130,7 @@ const componentModule = (elements) => {
       tags.forEach(tag => {
         tag.addEventListener('click', (event) => {
           event.stopPropagation();
-          const tagValue = tag.getAttribute("data-value");
+          const tagValue = tag.getAttribute('data-value');
           const checkbox = selectComponentWrapper.querySelector(`input[value="${tagValue}"]`);
           checkbox.click();
           const option = selectComponentWrapper.querySelector(`option[value="${tagValue}"]`);
@@ -155,7 +172,7 @@ const componentModule = (elements) => {
       const singleSelectTagSelect = () => {
         const tags = selectComponentWrapper.querySelectorAll('.pos-select__tags > div');
         tags.forEach(tag => {
-          const tagValue = tag.getAttribute("data-value");
+          const tagValue = tag.getAttribute('data-value');
           if (tagValue == nativeSelect.value) {
             tag.classList.remove('hidden');
           } else {
@@ -163,9 +180,9 @@ const componentModule = (elements) => {
           }
         });
 
-        const options = selectComponentWrapper.querySelectorAll('.pos-select__options > div');
+        const options = selectComponentWrapper.querySelectorAll('.pos-select__option');
         options.forEach(option => {
-          const optionValue = option.getAttribute("data-value");
+          const optionValue = option.getAttribute('data-value');
           if (optionValue == nativeSelect.value) {
             option.classList.add('bg-highlighted');
           } else {
@@ -181,8 +198,8 @@ const componentModule = (elements) => {
       };
 
       /* single select - custom select*/
-      selectComponentWrapper.querySelectorAll('.pos-select__options > div').forEach(option => {
-        const value = option.getAttribute("data-value");
+      selectComponentWrapper.querySelectorAll('.pos-select__option').forEach(option => {
+        const value = option.getAttribute('data-value');
         option.addEventListener('click', (event) => {
           nativeSelect.value = value;
           const changeevent = new Event('change');
@@ -196,9 +213,41 @@ const componentModule = (elements) => {
       nativeSelect.addEventListener('change', () => {
         singleSelectTagSelect();
       });
-    }
+    };
+
+    /* filtering the options list */
+    const filterClear = () => {
+      filter.value = '';
+
+      const optionsHidden = selectComponentWrapper.querySelectorAll('.pos-select__option.hidden');
+
+      optionsHidden.forEach(option => {
+        option.classList.remove('hidden');
+      });
+    };
+
+    if(filter){
+      const optionsAvailable = selectComponentWrapper.querySelectorAll('.pos-select__option');
+
+      filter.addEventListener('keyup', (event) => {
+        if(event.key === 'ArrowDown'){
+          event.preventDefault();
+          head.focus();
+        }
+
+        optionsAvailable.forEach(option => {
+          if(option.textContent.toLowerCase().trim().includes(event.target.value.toLowerCase().trim())){
+            option.classList.remove('hidden');
+          } else {
+            option.classList.add('hidden');
+          }
+        });
+      });
+    };
+
 
   });
+
 };
 
 export default componentModule;
